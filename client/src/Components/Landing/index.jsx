8f1978d79b7b8b1bd79f2fb517038b003bcd137f
Jamer Rebolledo <jamerrq@gmail.com>
2023-07-03 21:52:07 -0500

    last deploy changes | via Alk

--- client/src/Components/Landing/index.jsx
+++ client/src/Components/Landing/index.jsx
@@ -18,7 +18,11 @@ class Landing extends React.Component {
             <div className='landing'>
                 {/* <h1 className='title'>LANDING PAGE</h1> */}
                 <NavLink to='/home'>
-                    <button id="landing-button"><TbDog />&nbsp;GO HOME&nbsp;<TbDog /></button>
+                    <button id="landing-button">
+                        <TbDog style={{ fontSize: "22px" }} />
+                        &nbsp;GO HOME&nbsp;
+                        <TbDog style={{ fontSize: "22px" }} />
+                    </button>
                 </NavLink>
             </div>
         );
